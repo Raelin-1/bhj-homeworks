8ade77c34e904ef35e52cc454459131061ffdb46
Anastasia HTML <93232014+Raelin-1@users.noreply.github.com>
2024-09-25 20:27:32 +0100

    first commit popup

---
+++ client-state/popup/task.js
@@ -0,0 +1,19 @@
+const modalClose = document.querySelector(".modal__close");
+const modal = document.querySelector(".modal");
+
+if (!getCookie("modalUnopened")) {
+  modal.classList.add("modal_active");
+}
+
+modalClose.addEventListener("click", () => {
+  modal.classList.remove("modal_active");
+  document.cookie = "modalUnopened=modal";
+});
+
+function getCookie(name) {
+  const value = "; " + document.cookie;
+  let parts = value.split("; " + name + "=");
+  if (parts.length === 2) {
+    return parts.pop().split(";").shift();
+  }
+}
