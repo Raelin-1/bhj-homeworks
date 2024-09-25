aed26791b181eae09317cc909082c3b2163b2a73
Anastasia HTML <93232014+Raelin-1@users.noreply.github.com>
2024-09-25 20:12:49 +0100

    first commit text-editor

---
+++ client-state/text-editor/task.js
@@ -0,0 +1,13 @@
+const editor = document.getElementById("editor");
+const button = document.getElementById("button");
+// проверка на наличие значения в textarea
+editor.value = localStorage.getItem("editorValue");
+// добавлене значения textarea в localStorage
+editor.addEventListener("input", () => {
+  localStorage.setItem("editorValue", editor.value);
+});
+// удаление localStorage и значения textarea
+button.onclick = function () {
+  localStorage.removeItem("editorValue");
+  editor.value = "";
+};
